50d66aa79f46f3d915c8592064c1bb5ef9712975
abbylow <yu950713@gmail.com>
2021-12-07 16:50:51 +0800

    add solution

--- hackerrank/CycleDetection/solution.js
+++ hackerrank/CycleDetection/solution.js
@@ -1,3 +1,136 @@
-// Cycle Detection
-// Question: https://www.hackerrank.com/challenges/detect-whether-a-linked-list-contains-a-cycle/problem
-// Answer: https://www.youtube.com/watch?v=MFOAbpfrJ8g&ab_channel=HackerRank
+/* Cycle Detection
+ * Question: https://www.hackerrank.com/challenges/detect-whether-a-linked-list-contains-a-cycle/problem
+ * Solution: https://www.youtube.com/watch?v=MFOAbpfrJ8g&ab_channel=HackerRank
+ * Note: cannot run this question on Hackerrank, due to cannot reassign the temp / extra variable when they are declared as constant
+ */
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+// Complete the hasCycle function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function hasCycle(head) {
+    if (head == null) return 0;
+    
+    let slow = head;
+    let fast = head.next;
+    
+    while(slow && fast && fast.next) {
+        if (slow == fast) {
+            return 1;
+        }
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return 0;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const tests = parseInt(readLine(), 10);
+
+    for (let testsItr = 0; testsItr < tests; testsItr++) {
+        const index = parseInt(readLine(), 10);
+
+        const llistCount = parseInt(readLine(), 10);
+
+        let llist = new SinglyLinkedList();
+
+        for (let i = 0; i < llistCount; i++) {
+            const llistItem = parseInt(readLine(), 10);
+            llist.insertNode(llistItem);
+        }
+      
+      	let extra = new SinglyLinkedListNode(-1);
+        let temp = llist.head;
+
+        for (let i = 0; i < llistCount; i++) {
+          	if (i == index) {
+            	extra = temp;
+          	}
+
+          	if (i != llistCount-1) {
+            	temp = temp.next;
+          	}
+        }
+
+        temp.next = extra;
+
+        let result = hasCycle(llist.head);
+
+        ws.write((result ? 1 : 0) + "\n");
+    }
+
+    ws.end();
+}
